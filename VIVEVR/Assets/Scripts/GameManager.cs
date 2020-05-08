d709e1ed4ccd07e3e30d4f04525b50c39f1b445c
KID <50666207+KID421@users.noreply.github.com>
2020-05-08 10:40:23 +0800

    完成

--- VIVEVR/Assets/Scripts/GameManager.cs
+++ VIVEVR/Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;  // 引用 場景管理器 API
 using Valve.VR.InteractionSystem;   // 引用 VR 互動 API
 
 public class GameManager : MonoBehaviour
@@ -9,9 +9,24 @@ public class GameManager : MonoBehaviour
     public Text textBallCount;
     [Header("分數")]
     public Text textScore;
+    [Header("兩分球音效")]
+    public AudioClip soundTwo;
+    [Header("三分球音效")]
+    public AudioClip soundThree;
 
+    private AudioSource aud;
     private int ballCount = 5;
     private int score;
+
+    private ThreePoint threePoint;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+
+        // 三分球物件 = 透過類型尋找物件<類型>() - 當要尋找的類型場景上只有一個適用
+        threePoint = FindObjectOfType<ThreePoint>();
+    }
 
     public void UseBall(GameObject ball)
     {
@@ -28,19 +43,31 @@ public class GameManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // 分數遞增2
-        score+=2;
+        // 如果 三分球物件.在三分球區域為勾選
+        if (threePoint.inThreePoint)
+        {
+            score += 3;                             // 分數遞增3
+            aud.PlayOneShot(soundThree, 1.5f);      // 播放音效
+        }
+        else
+        {
+            score += 2;                             // 分數遞增2
+            aud.PlayOneShot(soundTwo);              // 播放音效
+        }
+        
         // 更新文字
         textScore.text = "分數：" + score;
     }
 
     public void Replay()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // 場景管理器.載入場景("場景名稱")
+        SceneManager.LoadScene("投籃機");
     }
 
     public void Quit()
     {
+        // 應用程式.離開遊戲() - 適用於 PC、手機
         Application.Quit();
     }
 }
---
+++ VIVEVR/Assets/Scripts/ThreePoint.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ThreePoint : MonoBehaviour
+{
+    private Text textArea;
+
+    /// <summary>
+    /// 進入三分球區域：true 進入、false 離開
+    /// </summary>
+    public bool inThreePoint;
+
+    private void Start()
+    {
+        textArea = GameObject.Find("區域").GetComponent<Text>();
+    }
+
+    /// <summary>
+    /// Enter 物件進入時
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
+    {
+        // 如果碰到的物件名稱為 "HeadCollider"
+        if (other.name == "HeadCollider")
+        {
+            inThreePoint = true;
+            textArea.text = "區域：三分球";
+        }
+    }
+
+    /// <summary>
+    /// Exit 物件離開時
+    /// </summary>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "HeadCollider")
+        {
+            inThreePoint = false;
+            textArea.text = "區域：兩分球";
+        }
+    }
+}
